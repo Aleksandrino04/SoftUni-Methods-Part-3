4180e38587aa0e186143724849b942258d389ca2
Aleksandrino04 <106624793+Aleksandrino04@users.noreply.github.com>
2022-06-15 17:06:06 +0300

    Add files via upload

---
+++ 11. Math operations/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _11._Math_operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double firstNumber = int.Parse(Console.ReadLine());
+            string @operator = Console.ReadLine();
+            double secondNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine(Calculate(firstNumber, @operator, secondNumber));
+
+            static double Calculate(double firstNumber, string @operator, double secondNumber)
+            {
+                double result = 0;
+                switch (@operator)
+                {
+                    case "+":
+                        result = firstNumber + secondNumber;
+                            break;
+                    case "-":
+                        result = firstNumber - secondNumber;
+                        break;
+                    case "*":
+                        result = firstNumber * secondNumber;
+                        break;
+                    case "/":
+                        result = firstNumber / secondNumber;
+                        break;
+                }
+                return result;
+
+            }
+        }
+    }
+}
